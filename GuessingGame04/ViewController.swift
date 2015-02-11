02ba6ac16d9c7b7172fa5310aae889e0f758cb6a
David M Gudeman <davidmgudeman@gmail.com>
2015-02-11 08:56:02 -0800

    starter program

--- GuessingGame04/ViewController.swift
+++ GuessingGame04/ViewController.swift
@@ -9,17 +9,70 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+  var guesses = 0
+  var numberToGuess = 0
+  var gameOver = false
+  
+  
+  @IBOutlet var userInput: UITextField!
+  @IBOutlet var gameOutput: UILabel!
+  
+  @IBAction func userGuess(sender: AnyObject) {
+    if gameOver {
+      var newGame = userInput.text
+      if newGame == "Y" {
+        gameOver = false
+        gameOutput.text = ""
+        clearInput()
+        numberToGuess = generateNumber()
+        showOutput( "\n\nI'm thinking of a number....\n")
+        return
+      }
+    }
+    var possibleGuess : Int? = userInput.text.toInt()
+    if let guess = possibleGuess {
+      if guess > numberToGuess {
+        showOutput("\(guess): You guessed to high!")
+      } else if guess < numberToGuess {
+        showOutput("\(guess): You guessed to low")
+      } else {
+        showOutput("\n\(guess): You win!\n")
+        showOutput("Go again? (Y)")
+        guesses = 0
+        gameOver = true
+      }
+      clearInput()
+    } else {
+      showOutput("Please put in a valid number\n")
+      clearInput()
+    }
+  }
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    numberToGuess = generateNumber()
+    showOutput("I'm thinking of a number....\n")
   }
-
+  
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
-
-
+  
+  func showOutput(outText: String) {
+    gameOutput.text = gameOutput.text! + outText
+  }
+  
+  func generateNumber() -> Int {
+    return random() % 100 + 1
+  }
+  func clearInput() {
+    userInput.text = ""
+  }
+  
+  
 }
+
+
+
 
